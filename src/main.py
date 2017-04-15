1af84f3bbf332b3c222d69bb16d8e3e500b71801
Naveen Tripathi <naveen.tripathi1010@gmail.com>
2017-04-15 14:23:10 +0530

    thresholding added

--- src/main.py
+++ src/main.py
@@ -5,12 +5,25 @@ import cv2
 import rospy
 from geometry_msgs.msg import Twist
 
+def cb(x):
+	pass
+
 def main():
 	# pub = rospy.Publisher('angles', Twist, queue_size=10)
 	rospy.init_node('feeder', anonymous=True)
 	rospy.loginfo("Program has started!!")
 
 	cap = cv2.VideoCapture(0)
+	cv2.namedWindow("Direct Feed", cv2.WINDOW_NORMAL)
+	cv2.namedWindow("Detected", cv2.WINDOW_NORMAL)
+	cv2.namedWindow("Settings", cv2.WINDOW_NORMAL)
+
+	cv2.createTrackbar("HUE L", "Settings", 0, 179, cb)
+	cv2.createTrackbar("HUE H", "Settings", 179, 179, cb)
+	cv2.createTrackbar("SAT L", "Settings", 0, 255, cb)
+	cv2.createTrackbar("SAT H", "Settings", 255, 255, cb)
+	cv2.createTrackbar("VAL L", "Settings", 0, 255, cb)
+	cv2.createTrackbar("VAL H", "Settings", 255, 255, cb)
 
 	while(cap.isOpened() and not rospy.is_shutdown()):
 		check, frame = cap.read()
@@ -19,7 +32,21 @@ def main():
 			rospy.logerr("Couldnt get feed from the camers. Exiting!")
 			break
 		elif(check == True):
+			# Direct feed from camera
 			cv2.imshow("Direct Feed", frame)
+
+			# converting the rgb image into hsv color space
+			hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+			# getting the values of HSV trackbars
+			hsv_l = (cv2.getTrackbarPos("HUE L", "Settings"), cv2.getTrackbarPos("SAT L", "Settings"), cv2.getTrackbarPos("VAL L", "Settings"))
+			hsv_h = (cv2.getTrackbarPos("HUE H", "Settings"), cv2.getTrackbarPos("SAT H", "Settings"), cv2.getTrackbarPos("VAL H", "Settings"))
+			
+			# creating a binary image using the HSV values
+			binary_frame = cv2.inRange(hsv_frame, hsv_l, hsv_h)
+
+			cv2.imshow("Detected", binary_frame)
+
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
 
